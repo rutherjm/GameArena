b1eb63fe4c0ef3b58c676bf94ed73ee92e876b9b
Rutherford <rutherjm@lancs.local>
2017-02-21 11:32:23 +0000

    tidied up code, implemented game arena

--- Driver.java
+++ Driver.java
@@ -1,13 +1,20 @@
 public class Driver
 {
+	private static int GAposX = 500; 
+	private static int GAposY = 500;
+	private static int[][] rectangleLoc = new int[100][2]; //for column, 0 = x, 1 = y
+
 	public static void main(String[] arguments)
 	{
 		initiateGameArena();
 	}
 	public static void initiateGameArena()
 	{
-		int[][] rectangleLoc = new int[100][2]; //for column, 0 = x, 1 = y
+		
+		rectangleLoc[0][0] = 100; //X coord
+		rectangleLoc[0][1] = 50; //Y coord
+		GameArena GA = new GameArena(GAposX,GAposY);
 
-
+		
 	}
 }
