73dd025f02aa6e7bedc63c246f67aa474ebe34f4
Rutherford <rutherjm@lancs.local>
2017-02-21 12:10:42 +0000

    idea changed. program now has one big rectangle with the paths that the user can follow ontop of it.

--- Driver.java
+++ Driver.java
@@ -4,22 +4,49 @@ public class Driver
 	private static int GAposY = 500;
 	private static int rectangleAmount = 1;
 	private static int[][] rLoc = new int[rectangleAmount][4]; //for column, 0 = x, 1 = y, 2 = w, 3 = h
-
+	private static GameArena GA = new GameArena(GAposX,GAposY);
 	public static void main(String[] arguments)
 	{
 		initiateGameArena();
 	}
 	public static void initiateGameArena()
 	{
-		GameArena GA = new GameArena(GAposX,GAposY);
+		Rectangle arenaColour = new Rectangle((GAposX - (GAposX / 2)),(GAposY - (GAposY / 2)),GAposX,GAposY, "BLUE");
+		GA.addRectangle(arenaColour);
+		//generateRectangles();
 
-		rLoc[0][0] = 250; //X coord
-		rLoc[0][1] = 0; //Y coord
-		rLoc[0][2] = 500; //W coord
-		rLoc[0][3] = 10; //H coord
+	}
+	public static void generateRectangles()
+	{
+		String colour = "BLUE";
 		
-		Rectangle R1 = new Rectangle(rLoc[0][0],rLoc[0][1],rLoc[0][2],rLoc[0][3], "CYAN");
+		rLoc[0][0] = 250; //X coord
+		rLoc[0][1] = 10; //Y coord
+		rLoc[0][2] = 500; //W coord
+		rLoc[0][3] = 20; //H coord
+		Rectangle R1 = new Rectangle(rLoc[0][0],rLoc[0][1],rLoc[0][2],rLoc[0][3], colour);
 		GA.addRectangle(R1);
+		
+		rLoc[0][0] = 250; //X coord
+		rLoc[0][1] = 490; //Y coord
+		rLoc[0][2] = 500; //W coord
+		rLoc[0][3] = 20; //H coord
+		Rectangle R2 = new Rectangle(rLoc[0][0],rLoc[0][1],rLoc[0][2],rLoc[0][3], colour);
+		GA.addRectangle(R2);
+		
+		rLoc[0][0] = 10; //X coord
+		rLoc[0][1] = 250; //Y coord
+		rLoc[0][2] = 20; //W coord
+		rLoc[0][3] = 500; //H coord
+		Rectangle R3 = new Rectangle(rLoc[0][0],rLoc[0][1],rLoc[0][2],rLoc[0][3], colour);
+		GA.addRectangle(R3);
+		
+		rLoc[0][0] = 490; //X coord
+		rLoc[0][1] = 250; //Y coord
+		rLoc[0][2] = 20; //W coord
+		rLoc[0][3] = 500; //H coord
+		Rectangle R4 = new Rectangle(rLoc[0][0],rLoc[0][1],rLoc[0][2],rLoc[0][3], colour);
+		GA.addRectangle(R4);
 	}
 	
 }
