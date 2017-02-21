51484ddaf44286b32cf4d8169e306395cd256853
Rutherford <rutherjm@lancs.local>
2017-02-21 11:52:14 +0000

    added h & w coord to  array.tested rectangle

--- Driver.java
+++ Driver.java
@@ -2,7 +2,8 @@ public class Driver
 {
 	private static int GAposX = 500; 
 	private static int GAposY = 500;
-	private static int[][] rectangleLoc = new int[100][2]; //for column, 0 = x, 1 = y
+	private static int rectangleAmount = 1;
+	private static int[][] rLoc = new int[rectangleAmount][4]; //for column, 0 = x, 1 = y, 2 = w, 3 = h
 
 	public static void main(String[] arguments)
 	{
@@ -10,11 +11,15 @@ public class Driver
 	}
 	public static void initiateGameArena()
 	{
-		
-		rectangleLoc[0][0] = 100; //X coord
-		rectangleLoc[0][1] = 50; //Y coord
 		GameArena GA = new GameArena(GAposX,GAposY);
 
+		rLoc[0][0] = 250; //X coord
+		rLoc[0][1] = 0; //Y coord
+		rLoc[0][2] = 500; //W coord
+		rLoc[0][3] = 10; //H coord
 		
+		Rectangle R1 = new Rectangle(rLoc[0][0],rLoc[0][1],rLoc[0][2],rLoc[0][3], "CYAN");
+		GA.addRectangle(R1);
 	}
+	
 }
